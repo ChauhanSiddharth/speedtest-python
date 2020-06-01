07685f988d786d8d45bbc6c9ca4dc2d474723d04
ChauhanSiddharth <siddharth.chauhan50@yahoo.com>
2020-05-31 23:49:26 -0400

    new template

--- app.py
+++ app.py
@@ -3,25 +3,24 @@ import speedtest
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-	return render_template('index.html')
-
-@app.route('/test')
-def test():
-	s = speedtest.Speedtest()
-	s.get_servers()
-	s.get_best_server()
-	s.download()
-	s.upload()
-	res = s.results.dict()
-	download = round(res["download"])/1000
-	upload = round(res["upload"])/1000
-	ping = round(res["ping"])
-	client = res["client"]["isp"]
-	country = res["client"]["country"]
-	print("-->Download Speed: {:.2f} Kb/s\n-->Upload Speed: {:.2f} Kb/s\n-->Ping: {}\n-->ISP: {}, {}".format(download,upload,ping, client,country))
-	return render_template('result.html', download=download, upload=upload,ping=ping,client=client)
+	if request.method == 'POST':
+		s = speedtest.Speedtest()
+		s.get_servers()
+		s.get_best_server()
+		s.download()
+		s.upload()
+		res = s.results.dict()
+		download = round(res["download"])/1000
+		upload = round(res["upload"])/1000
+		ping = round(res["ping"])
+		client = res["client"]["isp"]
+		country = res["client"]["country"]
+		print("-->Download Speed: {:.2f} Kb/s\n-->Upload Speed: {:.2f} Kb/s\n-->Ping: {}\n-->ISP: {}, {}".format(download,upload,ping, client,country))
+		return render_template('home.html', download=download, upload=upload,ping=ping,client=client)
+	else:
+		return render_template('home.html', download=0, upload=0,ping=0,client="None")	
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
